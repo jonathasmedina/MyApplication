0d68812cf69400de6024bb7fcc7bdb16e87885fa
Jonathas Leontino Medina <jonathas.medina@ifms.edu.br>
2016-04-15 18:17:07 -0400

    Initial commit

---
+++ app/src/main/java/com/example/asus/myapplication/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.asus.myapplication;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public void acao(View view) {
+
+        TextView txt;
+        txt = (TextView) findViewById(R.id.textView);
+
+        if (txt.getText() == "cliquei") {
+            txt.setText("não cliquei");
+        } else {
+            txt.setText("cliquei");
+        }
+
+    }
+
+
+}
